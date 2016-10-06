a69b66dfa311ab721519900eea86147cfa4596f2
Juanan <j_uanan@me.com>
2016-10-05 19:53:29 -0500

    v1.0.4
    Finished the glossary page. :<

--- js/memory.js
+++ js/memory.js
@@ -225,7 +225,7 @@
       <div class="mg__meta--right">\
       <button id="mg__button--restart" class="mg__button">Vuelve a comenzar</button>\
       <a href="comp_images.html" class="mg__falsebutton">Glosario de Imágenes.</a>\
-      <a href="conceptos.html" class="mg__falsebutton">Términos.</a>\
+      <a href="conceptos.html" class="mg__falsebutton">Glosario</a>\
       </div>';
     this.gameMeta.innerHTML = this.gameMetaHTML;
     this.game.appendChild(this.gameMeta);
